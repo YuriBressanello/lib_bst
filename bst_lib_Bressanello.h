8092874632ce03aae55154f84f1e278bfe5e797e
YuriBressanello <bressanelloyuri@gmail.com>
2024-11-27 22:58:33 +0100

    iterative and recursive search and iterative insert added

--- bst_lib_Bressanello.h
+++ bst_lib_Bressanello.h
@@ -10,6 +10,91 @@ class Node{
     public:
         Node(int k)
         : data {k}, lchild {nullptr}, rchild {nullptr}{};
+        bool searchI(int k){
+            Node *father = this;
+            Node *current = this;
+            bool check {true};
+                do{
+                    if(current==NULL){
+                        cout << "Node not found!!" << endl;
+                        return false;
+                        check = false;
+                    }
+                    if(current->data==k){
+                        cout << "Node found!!" << endl;
+                        return true;
+                        check = false;
+                    }
+                    if(k<current->data){
+                        father = current;
+                        current = current->lchild;
+                        check = true;
+                    } else {
+                        father = current;
+                        current = current->rchild;
+                        check = true;
+                    }
+            }while(check);
+
+            return true;
+
+        }
+        bool searchR(int k){
+            bool check {true};
+            do{
+                if(this==NULL){
+                    cout << "Node not found!!" << endl;
+                    return false;
+                    check = false;
+                }
+                if(this->data==k){
+                    cout << "Node found!!" << endl;
+                    return true;
+                    check = false;
+                }
+                if(k<this->data){
+                    return this->lchild->searchR(k);
+                } else {
+                    return this->rchild->searchR(k);
+                }
+            }while(check);
+
+            return true;
+
+        }
+        Node *insertI(int k){
+            Node *current = this;
+            Node *father = this;
+            bool check {true};
+            do{
+                if(current==NULL){
+                    if(k<father->data){
+                        father->lchild = new Node(k);
+                        return this;
+                        check = false;
+                    } else {
+                        father->rchild = new Node(k);
+                        return this;
+                        check = false;
+                    }
+                }
+                if(current->data==k){
+                    current->weight++;
+                    return current;
+                    check = false;
+                }
+                if(k<current->data){
+                    father = current;
+                    current = current->lchild;
+                } else {
+                    father = current;
+                    current = current->rchild;
+                }
+            }while(check);
+
+            return this;
+        
+        }
         Node *insertR(int k){
             if(this==NULL){
                 return new Node(k);
@@ -33,10 +118,14 @@ class Node{
             }
             return this;
         }
+        void preOrder();
         void inOrder(){
             if(this==NULL) return;
             this->lchild->inOrder();
             cout << this->data << " ";
             this->rchild->inOrder();
         }
+        void postOrder();
+        Node *deleteNode(int k);
+        bool isBst();
 };
